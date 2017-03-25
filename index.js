45bdb882a6def117a734a0d2ced02c706bfa1be6
Jozef Lang <developer@jozeflang.com>
2017-03-25 21:32:33 +0100

    Bootstrapping package

--- index.js
+++ index.js
@@ -4,5 +4,17 @@ const Homebridge, Accessory
 const request = require("request");
 
 module.exports = function(homebridge) {
-    console.log("Homebridge plugin is running!");
+    console.log("Homebridge plugin version '" + homebridge.version + "' is running!");
+    homebridge.registerPlatform("homebridge-inels", "inels", InelsPlatform, false);
 }
+
+function InelsPlatform(config, log) {
+    this.log = log;
+    log("InelsPlatform init");
+}
+
+InelsPlatform.prototype.accessories = function(callback) {
+    this.log("Getting Inels configuration");
+}
+
+
