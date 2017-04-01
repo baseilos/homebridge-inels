bcc34997f321aa08c55dcae4f4e5f6f2a6fbf894
cuong <nvcuong1982@yahoo.com>
2017-04-02 00:00:21 +0200

    Platform commits with real light bulb color , bright ness and saturation

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var Service;
 var Characteristic;
 var request = require('request');
 
+//Now import the light bulb class here.
+
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
@@ -12,124 +14,38 @@ module.exports = function (homebridge) {
 
 function Inels(log, config) {
     this.log = log;
-
     this.name            = config.name             || 'Inels Smart';
     this.type            = config.type;
 
-    this.host            = config.host             || '';
-    this.httpMethod      = config.http_method      || 'GET';
-    this.username        = config.username         || '';
-    this.password        = config.password         || '';
-    this.sendImmediately = config.send_immediately || '';
-
-    this.manufacturer    = config.manufacturer     || 'Inels';
-    this.model           = config.model            || 'Inels';
-
-    switch (this.type) {
-        case 'switch':
-            this.onUrl   = this.host + config.on;
-            this.offUrl  = this.host + config.off;
-            this.onBody  = config.on_body          || '';
-            this.offBody = config.off_body         || '';
-            break;
-
-        case 'multiswitch':
-            this.onBody  = config.on_body          || '';
-            this.offBody = config.off_body         || '';
-
-            this.multiswitch = config.multiswitch;
-            break;
-
-        default:
-            throw new Error('Unknown homebridge-inels switch type');
-    }
 }
 
 Inels.prototype = {
 
-    httpRequest: function(url, body, method, username, password, sendimmediately, callback) {
-        console.log(url,body,method);
-        request({
-                url: url,
-                json: body,
-                method: method,
-                headers: {
-                    'Content-Length': JSON.stringify(body).length,
-                    'Content-Type': 'application/json'
-                }
-
-            },
-            function(error, response, body) {
-                console.log(error, response.statusCode, response.statusMessage);
-                callback(error, response, body);
-            });
+    getStatus: function (callback) {
+        callback(null, true);
     },
 
-    setPowerState: function(targetService, powerState, callback, context) {
-        let funcContext = 'fromSetPowerState';
-        var reqUrl = '', reqBody = '';
-
-        if (context == funcContext) { // callback safety
-            if (callback) callback();
-            return;
-        }
-
-        switch(this.type) {
-            case 'switch':
-                if (!this.onUrl || !this.offUrl) {
-                    this.log.warn('Ignoring request; No power state urls defined.');
-                    callback(new Error('No power state urls defined.'));
-                    return;
-                }
-                this.log(powerState);
-
-                reqUrl  = powerState ? this.onUrl  : this.offUrl;
-                reqBody = powerState ? this.onBody : this.offBody;
-                this.log(reqBody, reqUrl);
-
-                break;
-
-            case 'multiswitch':
-                this.services.forEach(function (switchService, i) {
-                    if (i === 0) return; // skip informationService at index 0
-
-                    if (targetService.subtype === switchService.subtype) { // turn on
-                        reqUrl = this.host + this.multiswitch[i-1].path;
-                        switchService.getCharacteristic(Characteristic.On).setValue(true, undefined, funcContext);
-                    } else { // turn off
-                        switchService.getCharacteristic(Characteristic.On).setValue(false, undefined, funcContext);
-                    }
-                }.bind(this));
-                break;
-
-            default:
-                this.log('Unknown homebridge-switcheroo type in setPowerState');
-        }
-
-        this.httpRequest(reqUrl, reqBody, this.httpMethod, this.username, this.password, this.sendImmediately, function(error, response, responseBody) {
-            if (error) {
-                this.log.error('setPowerState failed: ' + error.message);
-                this.log('response: ' + response + '\nbody: ' + responseBody);
-
-                callback(error);
-            } else {
-                switch (this.type) {
-                    case 'switch':
-                        this.log.info('==> ' + (powerState ? "On" : "Off"));
-                        break;
-                    case 'multiswitch':
-                        this.log('==> ' + targetService.subtype);
-                        this.log.info('Body ==> ' + reqBody);
-
-                        break;
-                    default:
-                        this.log.error('Unknown type in request callback');
-                }
-
-                callback();
-            }
-        }.bind(this));
+    setPowerState: function (value, callback, context) {
+        callback();
     },
+
+    getBrightness: function (callback) {
+        callback(null, 50);
+    },
+
+    setBrightness: function (value, callback, context) {
+        callback();
+    },
+
+    getHue: function (callback) {
+        callback(null, 0.5);
+    },
+
+    setHue: function (value, callback, context) {
+        callback();
+    },
+
+
 
     identify: function (callback) {
         this.log('Identify me Senpai!');
@@ -145,47 +61,29 @@ Inels.prototype = {
             .setCharacteristic(Characteristic.Model, this.model);
         this.services.push(informationService);
 
-        switch (this.type) {
-            case 'switch':
-                this.log.warn('[Switch]: ' + this.name);
+        let switchService = new Service.Lightbulb(this.name);
 
-                let switchService = new Service.Switch(this.name);
-                switchService
-                    .getCharacteristic(Characteristic.On)
-                    .on('set', this.setPowerState.bind(this, switchService));
+        switchService
+            .getCharacteristic(Characteristic.On)
+            .on('set', this.setPowerState.bind(this))
+            .on('get', this.getStatus.bind(this));
+;
+        switchService
+            .getCharacteristic(Characteristic.Brightness)
+            .on('get', this.getBrightness.bind(this))
+            .on('set', this.setBrightness.bind(this));
 
-                this.services.push(switchService);
+        switchService
+            .getCharacteristic(Characteristic.Hue)
+            .on('get', this.getHue.bind(this))
+            .on('set', this.setHue.bind(this));
 
-                break;
-            case 'multiswitch':
-                this.log.warn('[Multiswitch]: ' + this.name);
+        switchService
+            .getCharacteristic(Characteristic.Saturation)
+            .on('get', this.getHue.bind(this))
+            .on('set', this.setHue.bind(this));
 
-                this.multiswitch.forEach(function(switchItem, i) {
-                    switch(i) {
-                        case 0:
-                            this.log.warn('---+--- ' + switchItem.name); break;
-                        case this.multiswitch.length-1:
-                            this.log.warn('   +--- ' + switchItem.name); break;
-                        default:
-                            this.log.warn('   |--- ' + switchItem.name);
-                    }
-
-                    let switchService = new Service.Switch(switchItem.name, switchItem.name);
-
-                    // Bind a copy of the setPowerState function that sets 'this' to the accessory and the first parameter
-                    // to the particular service that it is being called for.
-                    let boundSetPowerState = this.setPowerState.bind(this, switchService);
-                    switchService
-                        .getCharacteristic(Characteristic.On)
-                        .on('set', boundSetPowerState);
-
-                    this.services.push(switchService);
-                }.bind(this));
-
-                break;
-            default:
-                this.log('Unknown homebridge-switcheroo type in getServices');
-        }
+        this.services.push(switchService);
 
         return this.services;
     }
